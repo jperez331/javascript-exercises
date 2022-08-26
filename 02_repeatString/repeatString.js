50856bcb75a1e2e406ddb24d15cb6b87a84dfa5c
Jose Perez <font3face@gmail.com>
2022-08-26 02:04:46 -0500

    solve exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+function repeatString(str ,n) {
+  let finalStr = '';
+  if (n >= 0){
+    for (let i = 0; i < n; i++) {
+      finalStr += str;
+    }
+    return finalStr;
+  }else{
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
